e430523325035c3aaf125d70b1c3a16c8c431a29
Swapnanil Dowarah <swapnanildowarah10@gmail.com>
2026-08-09 17:05:03 +0000

    tried to make the domain work

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
 const isUserSite = repoName.endsWith(".github.io");
 const shouldUseBasePath = Boolean(process.env.GITHUB_ACTIONS) && repoName && !isUserSite;
-const basePath = shouldUseBasePath ? `/${repoName}` : "";
+const basePath = shouldUseBasePath ? `/` : "";
 
 const nextConfig: NextConfig = {
   output: "export",
